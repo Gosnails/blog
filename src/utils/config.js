4406eb7251498b9fb24a49f9ad1f65174d2c0c10
Songzhiguo <Fu_shide@163.com>
2019-09-12 10:58:53 +0800

    refactor: modify CATES

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,8 @@ export const BASE_IMG_URL = 'https://qiniu.fancyrobot.com/';
 export const CATES = [
     { label: '全部文章', value: '' },
     { label: 'Web开发', value: 'web' },
+    { label: 'Javascript', value: 'js' },
     { label: 'NodeJs', value: 'node' },
-    { label: '性能优化', value: 'optimize' },
+    { label: '开发手册', value: 'dev' },
     { label: '项目总结', value: 'summary' }
 ]
